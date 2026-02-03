bc1fbcdcba971980d115b2ef418f43160f77cd61
Hari.Dev <hariravi.dev.1004@gmail.com>
2026-02-03 11:11:33 +0530

    feat: add 2D Array  problem

---
+++ Basics/2DArray.java
@@ -0,0 +1,14 @@
+package Basics;
+
+class TwoDArray {
+    public static void main(String args[]) {
+        int[][] marks = {
+                { 100, 70, 23, 40, 17 },
+                { 20, 48, 59, 85, 69 }
+        };
+        System.out.println();
+        System.out.printf("English mark of the student 1 : %d%n", marks[0][0]);
+        System.out.printf("English mark of the student 2 : %d%n", marks[1][0]);
+
+    }
+}
