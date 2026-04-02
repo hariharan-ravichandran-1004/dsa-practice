4b737132ac8b8f6cd6c92029f9454f3a8df90f7e
Hari.Dev <hariravi.dev.1004@gmail.com>
2026-04-02 17:41:07 +0530

    feat: remaining advanced basics

--- Basics/advanced/Threading.java
+++
@@ -1,5 +0,0 @@
-package Basics.advanced;
-
-public class Threading {
-    
-}
--- Basics/advanced/lamdaDef.java
+++ Basics/advanced/lamdaDef.java
@@ -5,5 +5,14 @@ public class lamdaDef {
         System.out.println();
         Runnable r = () -> System.out.println("Hello");
         r.run();
-        }
+
+        InnerlamdaDef ild = (int a, int b) -> a * b;
+        int tot = ild.operate(10, 3);
+        System.out.println(tot);
+    }
+}
+
+@FunctionalInterface
+interface InnerlamdaDef {
+    int operate(int a, int b);
 }
