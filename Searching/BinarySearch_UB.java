1f581db9da6de8935eb877e98b01e67ae59544e6
Hari.Dev <hariravi.dev.1004@gmail.com>
2026-09-19 00:04:31 +0530

    feat: implement upper bound binary search

---
+++ Searching/BinarySearch_UB.java
@@ -0,0 +1,24 @@
+package Searching;
+
+public class BinarySearch_UB {
+    public static void main(String[] args) {
+        int target = 4;
+        int[] arr = { 1, 2, 3, 4, 4, 4, 5, 6 };
+        int low=0;
+        int high=arr.length;
+        int result = upperbound(low,high,arr,target);
+        System.out.println(result);
+    }
+
+    public static int upperbound(int low, int high, int[] arr, int target) {
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
